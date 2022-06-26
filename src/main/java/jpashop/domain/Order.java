02af23e72f20ce7f395038a2d8d067e8205625ea
meatsby <meatsby@gmail.com>
2022-06-26 22:36:45 +0900

    feat: implement relational multiplicity

--- src/main/java/jpashop/domain/Order.java
+++ src/main/java/jpashop/domain/Order.java
@@ -12,22 +12,25 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "ORDERS")
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-
-//    @Column(name = "MEMBER_ID")
-//    private Long memberId;
 
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
